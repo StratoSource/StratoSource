f5be4942c8393e9fe6aebc65f7d6ed7747d210e2
Mark Smith <mark@inline-six.com>
2016-09-23 16:01:10 -0400

    tracked down and removed the source of the annoying '0.8' output

--- stratosource/user/crontab.py
+++ stratosource/user/crontab.py
@@ -60,7 +60,6 @@ itemrex = re.compile('^\s*([^@#\s]+)\s([^@#\s]+)\s([^@#\s]+)\s([^@#\s]+)\s([^@#\
 specrex = re.compile('@(\w+)\s([^#\n]*)(\s+#\s*([^\n]*)|$)')
 devnull = ">/dev/null 2>&1"
 
-print version
 
 month_enum = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
 week_enum  = ['sun','mon','tue','wed','thu','fri','sat','sun']

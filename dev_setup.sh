03f69bd44a06ebf6d9e7b66e687b5ceba14cc225
Mark Smith <masmith@redhat.com>
2016-09-29 15:26:19 -0400

    Build & install process cleanup

--- dev_setup.sh
+++ dev_setup.sh
@@ -14,49 +14,47 @@ REL=`cat /etc/system-release`
 
 echo 'installing dependencies'
 
+FIRST_INSTALL='no'
+
 if [[ "$REL" =~ ^Fedora.* ]]; then
-	yum -y install python python-pip 
+	yum -y install python python-pip httpd mod_wsgi MySQL-python wget git cgit unzip mysql mariadb-server tzlocal
 	pip install --upgrade pip
-	pip install django django-bootstrap3 pyexcelerator pyral
-	yum -y install httpd python-suds python-requests python-memcached python-lxml mod_wsgi MySQL-python wget git cgit unzip mysql mariadb-server tzlocal
+	pip install -r resources/requirements.txt
 else
 	yum -y install epel-release
 	yum clean all
-	yum -y install python python-pip
+	yum -y install python python-pip httpd mod_wsgi MySQL-python wget git cgit unzip mysql mariadb-server tzlocal
 	pip install --upgrade pip
-	pip install django django-bootstrap3 pyexcelerator pyral
-	yum -y install httpd python-suds python-requests python-memcached python-lxml mod_wsgi MySQL-python wget git cgit unzip mysql mariadb-server tzlocal
+	pip install -r resources/requirements.txt
 
 fi
 
-echo 'configuring apache'
-
-eval grep django /etc/httpd/conf/httpd.conf
-# if apache not already configured for django, do it now
-if [ $? -eq 1 ]; then
-  SITEPKG=`python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"`
-  cat  < $TARGET/resources/httpd-append.conf >> /etc/httpd/conf/httpd.conf 
-  sed -i "s|PYTHON_SITEPKG|$SITEPKG|" /etc/httpd/conf/httpd.conf
+if [ ! -f /etc/httpd/conf.d/stratosource.conf ]; then
+  echo 'configuring apache'
+  cp resources/httpd.conf /etc/httpd/conf.d/stratosource.conf
+  FIRST_INSTALL='yes'
 fi
 
-echo 'configure mysql'
-# fix a bug in the mysql rpm installer that does not set correct permissions
-mysql_install_db
-chown -R mysql.mysql /var/lib/mysql
-touch /var/log/mysqld.log
-chown mysql:mysql /var/log/mysqld.log
-mkdir /var/run/mysqld
-chown mysql:mysql /var/run/mysqld
 
-echo 'configuring cgit'
-eval grep django /etc/cgitrc
-if [ $? -eq 1 ]; then
-  echo "include=$TARGET/resources/cgitrepo" >>/etc/cgitrc
-fi
-if [ ! -f /usr/django/cgitrepo ]; then
-  cp /usr/django/resources/cgitrepo /usr/django/stratosource
-  chmod 777 /usr/django/stratosource/cgitrepo
-fi
+if [ $FIRST_INSTALL == 'yes' ]; then
+  echo 'configure mysql'
+  # fix a bug in the mysql rpm installer that does not set correct permissions
+  mysql_install_db
+  chown -R mysql.mysql /var/lib/mysql
+  touch /var/log/mysqld.log
+  chown mysql:mysql /var/log/mysqld.log
+  mkdir /var/run/mysqld
+  chown mysql:mysql /var/run/mysqld
+
+  echo 'configuring cgit'
+  eval grep django /etc/cgitrc
+  if [ $? -eq 1 ]; then
+    echo "include=/var/sfrepo/config/cgitrepo" >>/etc/cgitrc
+  fi
+  if [ ! -f /var/sfrepo/config/cgitrepo ]; then
+    cp /usr/django/resources/cgitrepo /var/sfrepo/config
+    chmod 777 /var/sfrepo/config/cgitrepo
+  fi
 
 cp /usr/django/resources/memcached /etc/sysconfig
 mkdir $TARGET/logs
